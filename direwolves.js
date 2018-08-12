772973d28e214d7ee321571b80710dbc3d3d4f03
Katie <katie.segreti@gmail.com>
2018-08-11 23:06:03 -0400

    Create direwolves.js

---
+++ direwolves.js
@@ -0,0 +1,55 @@
+var w = 600;
+var h = 900;
+
+var starks = [
+	{
+		"name": "Robb",
+		"species": "human",
+		"sequence": 0
+	},
+	{
+		"name": "Greywind",
+		"species": "wolf",
+		"sequence": 0
+	},
+	{
+		"name": "Sansa",
+		"species": "human",
+		"sequence": 1
+	},
+	{
+		"name": "Lady",
+		"species": "wolf",
+		"sequence": 1
+	},
+	{
+		"name": "Arya",
+		"species": "human",
+		"sequence": 2
+	},
+	{
+		"name": "Nymeria",
+		"species": "wolf",
+		"sequence": 2
+	},
+	{
+		"name": "Bran",
+		"species": "human",
+		"sequence": 3
+	},
+	{
+		"name": "Summer",
+		"species": "wolf",
+		"sequence": 3
+	},
+	{
+		"name": "Rickon",
+		"species": "human",
+		"sequence": 4
+	},
+	{
+		"name": "Shaggy Dog",
+		"species": "wolf",
+		"sequence": 4
+	}
+]
